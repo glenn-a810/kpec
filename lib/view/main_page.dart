26345e00e113ab4e234e263928e8e6b1a2118d39
Glenn_M1 <a810.glenn@gmail.com>
2022-05-19 15:18:27 +0900

    Navigation 테스트. Login/Logout 라우팅 설정 테스트

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -7,7 +7,16 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('title area'),
+        title: Text('민방위 교육 출결 관리'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/login', (route) => false);
+            },
+            icon: Icon(Icons.logout),
+          )
+        ],
       ),
       body: Text('여기에 데이터 뿌리면 되겠네'),
     );
