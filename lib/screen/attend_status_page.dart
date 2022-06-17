7732aa8f5eff67af73571b86952d4aec41d0585f
Glenn <a810.glenn@gmail.com>
2022-06-17 19:56:17 +0900

    기획기반 UI배치를 위해 widget배열중. Sub Page생성

---
+++ lib/screen/attend_status_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AttendStatus extends StatefulWidget {
+  const AttendStatus({Key? key}) : super(key: key);
+
+  @override
+  State<AttendStatus> createState() => _AttendStatusState();
+}
+
+class _AttendStatusState extends State<AttendStatus> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screen/unconfirmed_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Unconfirmed extends StatefulWidget {
+  const Unconfirmed({Key? key}) : super(key: key);
+
+  @override
+  State<Unconfirmed> createState() => _UnconfirmedState();
+}
+
+class _UnconfirmedState extends State<Unconfirmed> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
