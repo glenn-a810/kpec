8b6187fab305d3b7058e3aed460c576b61abc577
Glenn <a810.glenn@gmail.com>
2022-06-28 20:46:55 +0900

    DatePicker추가, 선택한 값 state에 저장. 표시는 년,월,일까지만 해야할 듯

--- lib/screen/unconfirmed_page.dart
+++ lib/screen/unconfirmed_page.dart
@@ -8,22 +8,41 @@ class Unconfirmed extends StatefulWidget {
 }
 
 class _UnconfirmedState extends State<Unconfirmed> {
+  DateTime _setDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: () {
-        selectDate();
+        // selectDate();
+        Future selectedDate = showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(2022),
+          lastDate: DateTime(2023),
+        );
+
+        selectedDate.then((dateString) {
+          setState(() {
+            _setDate = dateString;
+          });
+        });
       },
-      child: Text('DatePicker'),
+      child: Column(
+        children: [
+          Text('DatePicker'),
+          Text('$_setDate'),
+        ],
+      ),
     );
   }
 
-  void selectDate() {
-    Future selectedDate = showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2023),
-    );
-  }
+  // void selectDate() {
+  //   Future selectedDate = showDatePicker(
+  //     context: context,
+  //     initialDate: DateTime.now(),
+  //     firstDate: DateTime.now(),
+  //     lastDate: DateTime(2023),
+  //   );
+  // }
 }
